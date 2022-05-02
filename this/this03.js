a16ea1a491ee6b7101c8bc086609dd2ef1bd7c2e
natanbravo <nobprogrammer8@gmail.com>
2022-05-02 17:57:44 -0300

    BUGFIX

--- this/this03.js
+++ this/this03.js
@@ -8,4 +8,4 @@ const car = new Car ("Honda");
 console.log(car.brand); //Output: Honda
 
 //The "this" inside of the "car"object makes reference to the object;
-//So, using a "." followed by some variable name "(this.brand)", automaticaly an atribute is created inside of the "car" object;
+//So, using a "." followed by some variable name (this.brand), automaticaly an atribute is created inside of the "car" object;
